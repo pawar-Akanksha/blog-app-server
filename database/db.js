e102b568d19f6695ac41de17fe3af288a31bb339
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-22 07:16:27 -0700

    Added files

--- database/db.js
+++ database/db.js
@@ -1,13 +1,6 @@
-import mongoose from 'mongoose';
 
-const Connection = async (URL) => {
-    
-    try {
-        await mongoose.connect(URL, { useNewUrlParser: true })
-        console.log('Database connected successfully');
-    } catch (error) {
-        console.log('Error while connecting to the database ', error);
-    }
-};
+MONGOURI = `mongodb+srv://akankshapawar:Blog123@cluster0.24rfbqs.mongodb.net/Blog?retryWrites=true&w=majority`,
+    ACCESS_SECRET_KEY = 'de37bbab03b0380009e4b2f9c6fa6dcf95ae3acfc63125ce7b580d8d490eb6f92579843a4ce44011998dbec04fab568e5de224b042b2c2cf284acfbe03f1f4a6',
+    REFRESH_SECRET_KEY = 'a60327222ffaa884573490fc0574d69f3f5d1925a98479fb4ef184ebc99fab84cbb9edcc41434b58a07b66188a75152916ed7b46b2031afa5b5c0f046cacb726'
 
-export default Connection;
+   export default MONGOURI;
--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-
-//components
-import Connection from './database/db.js';
 import Router from './routes/route.js';
-
-dotenv.config();
-
 const app = express();
+const PORT = 8000;
 
 app.use(cors());
 mongoose.set('strictQuery', false);
 mongoose.set('strictQuery', true)
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// import mongoose from 'mongoose';
+const MONGOURI = `mongodb+srv://akankshapawar:Blog123@cluster0.24rfbqs.mongodb.net/Blog?retryWrites=true&w=majority`;
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("connected to mongodb");
+}).catch((err)=>{
+    console.log("err in connection",err);
+})
+
+
+
+
+
+
 app.use('/', Router);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static("client/build"))
-}
-const PORT = process.env.PORT || 8000;
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static("client/build"))
+// }
 
-const URL = process.env.MONGODB_URI || `mongodb+srv://akankshapawar:Blog123@cluster0.24rfbqs.mongodb.net/Blog?retryWrites=true&w=majority`;
 
-Connection(URL);
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
