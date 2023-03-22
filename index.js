6e47bdecf33b8a27737ed769e1b83f0b2e5f8c5d
pawar-Akanksha <pawarakanksha942@gmail.com>
2023-03-22 04:11:30 -0700

    Added changes

--- index.js
+++ index.js
@@ -23,9 +23,8 @@ if(process.env.NODE_ENV === "production"){
     app.use(express.static("client/build"))
 }
 const PORT = process.env.PORT || 8000;
-const USERNAME = process.env.DB_USERNAME;
-const PASSWORD = process.env.DB_PASSWORD;
-const URL = process.env.MONGODB_URI || `mongodb+srv://${USERNAME}:${PASSWORD}@cluster0.24rfbqs.mongodb.net/Blog?retryWrites=true&w=majority`;
+
+const URL = process.env.MONGODB_URI || `mongodb+srv://akankshapawar:Blog123@cluster0.24rfbqs.mongodb.net/Blog?retryWrites=true&w=majority`;
 
 Connection(URL);
 
